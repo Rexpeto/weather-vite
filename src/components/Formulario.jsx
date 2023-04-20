717bfc0e88b028c01e7232fbe04f0b9947f2007f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 15:09:08 -0400

    Update styles

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -63,7 +63,7 @@ const Formulario = () => {
                 </div>
                 <button
                     type="Submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition duration-150"
+                    className="text-white bg-blue-700 hover:bg-blue-800 outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
                 >
                     Consultar
                 </button>
