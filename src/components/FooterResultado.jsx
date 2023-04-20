6064182150f56766d2a48fc47007bb772fd258a0
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 15:43:08 -0400

    Footer temp

---
+++ src/components/FooterResultado.jsx
@@ -0,0 +1,36 @@
+import {
+    RiCelsiusLine,
+    RiTempColdLine,
+    RiTempHotLine,
+    RiCloudyLine,
+    RiCloudWindyLine,
+} from "react-icons/ri";
+
+const FooterResultado = ({ temp, cloud }) => {
+    const kelvin = 273.15;
+
+    return (
+        <div className="flex justify-between items-center gap-2 w-full mt-8 px-5">
+            <div className="flex items-center text-lg">
+                <RiTempColdLine title="Temperatura mínima" />
+                <p className="mr-1 ml-2">{parseInt(temp?.temp_min - kelvin)}</p>
+                <RiCelsiusLine />
+            </div>
+            <div className="flex items-center text-lg">
+                <RiTempHotLine title="Temperatura máxima" />
+                <p className="mr-1 ml-2">{parseInt(temp?.temp_max - kelvin)}</p>
+                <RiCelsiusLine />
+            </div>
+            <div className="flex items-center gap-2 text-lg">
+                <RiCloudyLine title="Nubes" />
+                <p>{cloud}%</p>
+            </div>
+            <div className="flex items-center gap-2 text-lg">
+                <RiCloudWindyLine title="Humedad" />
+                <p>{temp?.humidity}%</p>
+            </div>
+        </div>
+    );
+};
+
+export default FooterResultado;
