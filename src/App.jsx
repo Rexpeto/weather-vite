d3b3adb7dcca57cd34617b2ff261f41ba5caa8bb
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 10:29:57 -0400

    Use ClimaProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import AppClima from "./components/AppClima";
+import { ClimaProvider } from "./context/ClimaProvider";
 
 const App = () => {
     return (
-        <div className="container mx-auto py-4">
-            <h1 className="font-bold text-4xl text-center mb-8">Open Weather + Vite</h1>
-            <AppClima />
-        </div>
+        <ClimaProvider>
+            <div className="container mx-auto py-4">
+                <h1 className="font-bold text-4xl text-center mb-8">
+                    Open Weather + Vite
+                </h1>
+                <AppClima />
+            </div>
+        </ClimaProvider>
     );
 };
 
