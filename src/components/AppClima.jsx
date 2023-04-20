e5357da89ab33a17732095e92bcc92abd439a578
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 11:29:04 -0400

    Use toastify

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -1,8 +1,11 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Formulario from "./Formulario";
 
 const AppClima = () => {
     return (
         <>
+            <ToastContainer theme="dark" />
             <main className="grid grid-cols-1 gap-8 md:grid-cols-2">
                 <Formulario />
                 <div>Resultado</div>
