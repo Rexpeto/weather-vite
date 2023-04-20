723aaf88f091e67ee2a48d877e29378fa449c2f6
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 09:57:42 -0400

    Layout grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import AppClima from "./components/AppClima";
 
 const App = () => {
-    return <AppClima />;
+    return (
+        <div className="container mx-auto py-4">
+            <h1 className="font-bold text-4xl text-center mb-8">Open Weather + Vite</h1>
+            <AppClima />
+        </div>
+    );
 };
 
 export default App;
