398abb9bf80cecdba5500ff4bb44a25d1a93b374
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 15:42:45 -0400

    Mostrando resultados

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,28 @@
+import useClima from "../hooks/useClima";
+import { RiCelsiusLine } from "react-icons/ri";
+import FooterResultado from "./FooterResultado";
+
+const Resultado = () => {
+    const { respuesta } = useClima();
+    const {
+        clima: { main },
+        clima: { clouds },
+    } = respuesta;
+
+    //* Grados Kelvin
+    const kelvin = 273.15;
+
+    return (
+        <div className="flex flex-col items-center gap-3 max-w-full p-6 rounded-lg shadow bg-gray-800/90 border-gray-700 backdrop-blur-xl backdrop-brightness-75">
+            <h2 className="font-bold text-2xl">{respuesta?.name}</h2>
+            <p className="font-bold text-xl">{respuesta?.state}</p>
+            <div className="flex gap-2">
+                <h3 className="text-4xl">{parseInt(main?.temp - kelvin)}</h3>
+                <RiCelsiusLine className="text-4xl" />
+            </div>
+            <FooterResultado temp={main} cloud={clouds.all} />
+        </div>
+    );
+};
+
+export default Resultado;
