89a570ca49b3f24654b25a383dbd7a022fcbf91f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 15:45:09 -0400

    Usando Resultado

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -1,14 +1,17 @@
+import useClima from "../hooks/useClima";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Formulario from "./Formulario";
+import Resultado from "./Resultado";
 
 const AppClima = () => {
+    const { respuesta } = useClima();
     return (
         <>
             <ToastContainer theme="dark" />
             <main className="grid grid-cols-1 gap-8 md:grid-cols-2">
                 <Formulario />
-                <div>Resultado</div>
+                {respuesta?.name ? <Resultado /> : null}
             </main>
         </>
     );
