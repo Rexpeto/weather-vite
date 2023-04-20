91031e6f8326654d09fec5ce3c6707f378c271fb
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 10:29:20 -0400

    Use ClimaProvider

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -3,7 +3,7 @@ import Formulario from "./Formulario";
 const AppClima = () => {
     return (
         <>
-            <main className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <main className="grid grid-cols-1 gap-8 md:grid-cols-2">
                 <Formulario />
                 <div>Resultado</div>
             </main>
