a890974543d42c2bf10cef9f419ae680dbce277f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-20 09:24:15 -0400

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import AppClima from "./components/AppClima";
+
+const App = () => {
+    return <AppClima />;
+};
+
+export default App;
---
+++ src/components/AppClima.jsx
@@ -0,0 +1,12 @@
+const AppClima = () => {
+    return (
+        <>
+            <main className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <div>Formulario</div>
+                <div>Resultado</div>
+            </main>
+        </>
+    );
+};
+
+export default AppClima;
